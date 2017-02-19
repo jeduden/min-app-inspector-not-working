a77761e826e43cc732d9a7167bb81314f735e14a
Jan-Eric Duden <jeduden@gmail.com>
2017-02-19 11:19:10 +0100

    Initial Commit

---
+++ no-app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  no-app
+//
+//  Created by Jan-Eric Duden on 19/02/2017.
+//  Copyright © 2017 Jan-Eric Duden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ no-app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  no-app
+//
+//  Created by Jan-Eric Duden on 19/02/2017.
+//  Copyright © 2017 Jan-Eric Duden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
